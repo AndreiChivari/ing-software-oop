2e8b0fb7a1b87d61cdee7d6f60da8a41cc6a8b2f
chivari.andrei <andrei.chivari@gmail.com>
2026-02-03 22:31:00 +0200

    First version

---
+++ Gadget.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ING_SOFT_OOP2
+{
+    interface IBluetooth
+    {
+        void ConecteazaBluetooth();
+    }
+
+    class Gadget
+    {
+        public string Model { get; set; }
+        public int Pret { get; set; }
+
+        public Gadget(string model, int pret)
+        {
+            Model = model;
+            Pret = pret;
+        }
+
+        public virtual void Porneste()
+        {
+            Console.WriteLine("Gadgetul porneste...");
+        }
+    }
+
+    class Telefon : Gadget, IBluetooth
+    {
+        public string SistemDeOperare { get; set; }
+
+        public Telefon(string model, int pret, string os): base(model, pret)
+        {
+            SistemDeOperare = os;
+        }
+
+        public override void Porneste()
+        {
+            Console.WriteLine($"Telefonul {Model} afiseaza logo-ul {SistemDeOperare}");
+        }
+
+        public void ConecteazaBluetooth()
+        {
+            Console.WriteLine("Telefonul s-a conectat la casti!");
+        }
+
+
+    }
+
+    class Frigider : Gadget
+    {
+        public string ClasaEnergetica { get; set; }
+
+        public Frigider(string model, int pret, string clasaEnergetica) : base(model, pret)
+        {
+            ClasaEnergetica = clasaEnergetica;
+        }
+
+        public override void Porneste()
+        {
+            Console.WriteLine($"Frigiderul {Model} a pornit compresorul");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ING_SOFT_OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefon tel1 = new Telefon("IPhone 15", 5000, "IOS");
+            Frigider frig1 = new Frigider("Samsung", 4500, "A++");
+            List<Gadget> gadgets = new List<Gadget>();
+            gadgets.Add(tel1);
+            gadgets.Add(frig1);
+
+            foreach(Gadget g in gadgets)
+            {
+                g.Porneste();
+
+                if (g is IBluetooth b)
+                {
+                    b.ConecteazaBluetooth();
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
+ 
